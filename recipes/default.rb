84172c880837c76eb124ae90c1aaba59b9fe3e92
PhilBug <filip.bugaj13@gmail.com>
2019-06-01 21:23:21 +0200

    task 1 - solved

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+group 'tsd' do
+  gid 1234
+
+  action :create
+end
+
+user 'tsd' do
+  comment 'This is user created by chef'
+  uid 1234
+  gid 1234
+  home '/home/tsd'
+  shell '/bin/bash'
+  password 'admin'
+  manage_home true
+
+  action :create
+end
+
+file '/home/tsd/tsd-testfile' do
+  content 'This file was created by chef'
+  mode '754'
+  owner 'tsd'
+  group 'tsd'
+
+  action :create
+end
